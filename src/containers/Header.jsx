dc37ba8710d709ef908c3c5bf2332e5d1b3e7fb0
John Robling <roblingjohn@gmail.com>
2023-11-01 19:38:06 -0400

    Adds basics of header.

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
     <div>
-      <h1>This is the header.</h1>
+      <Navbar bg='dark' expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">
+            <h1>Red Oak District</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="me-auto">
+              <Nav.Link href="/program">The Program</Nav.Link>
+              <Nav.Link href="/neighborhood">The Neighborhood</Nav.Link>
+              <Nav.Link href="/future">The Future</Nav.Link>
+              <Nav.Link href="/events">The Events</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
