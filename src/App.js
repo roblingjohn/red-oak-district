60d6200cf2ffb74999099a5ac8d8318666ee57ab
John Robling <roblingjohn@gmail.com>
2023-11-01 19:23:43 -0400

    Lays out basic components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import Home from "./containers/Home";
+import Footer from "./containers/Footer";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" component={Home} />
+        </Routes>
+        <h1>This is the home page.</h1>
+      </Router>
+      <Footer />
     </div>
   );
 }
