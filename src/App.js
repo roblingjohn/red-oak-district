28bcd75c963ff8db8baae469ba11f39e134ab6d1
John Robling <roblingjohn@gmail.com>
2023-11-05 19:31:41 -0500

    Moves footer to components folder.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./containers/Header";
 import Home from "./containers/Home";
-import Footer from "./containers/Footer";
+import Footer from "./components/Footer";
 import Program from "./containers/Program";
 import Neighborhood from "./containers/Neighborhood";
 import Future from "./containers/Future";
