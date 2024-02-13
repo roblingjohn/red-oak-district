232af1cd768c77c19c08ff331c2ddf67e9c51fee
John Robling <roblingjohn@gmail.com>
2024-02-12 20:38:42 -0500

    Adds copy to arts page.

--- src/containers/Arts.jsx
+++ src/containers/Arts.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Arts = () => {
   return (
-    <div>
+    <Container>
       <h1>Arts and Culture</h1>
-    </div>
+      <p>
+        The Red Oak District is a pioneering creative placemaking project
+        located in the heart of South Fulton, Georgia. This innovative
+        initiative seeks to transform the historic downtown area into a vibrant
+        hub of culture, commerce, and community engagement.
+      </p>
+      <p>
+        At its core, the Red Oak District aims to preserve the rich heritage of
+        the region while embracing the spirit of creativity and innovation.
+        Through strategic revitalization efforts, the project breathes new life
+        into historic buildings and public spaces, blending the past with the
+        present in a harmonious fusion.
+      </p>
+      <p>
+        One of the defining features of the Red Oak District is its commitment
+        to community involvement. Residents, business owners, and stakeholders
+        are actively engaged in the planning and implementation process,
+        ensuring that the project reflects the unique identity and aspirations
+        of the local population.
+      </p>
+      <p>
+        Art and culture play a central role in the Red Oak District, with public
+        art installations, cultural events, and creative programming enriching
+        the urban landscape. These initiatives not only enhance the aesthetic
+        appeal of the area but also foster a sense of belonging and pride among
+        residents.
+      </p>
+
+      <ul>
+        <li>Mural Project </li>
+        <li>Red Oak Art Festival </li>
+        <li>Newspaper Stand (Need to get pictures) </li>
+        <li>Public Art Incubator (Connect to Public Art Page when Live)</li>
+      </ul>
+    </Container>
   );
 };
 
